ee001c7cd90fe8aa18773cbd045cb07848ad6785
Admin <josejordan20222@gmail.com>
2026-01-25 01:32:51 +0000

    Fix config syntax: next.config.mjs, node 22.13.0, prisma fallback

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    // Esta parte soluciona los errores de dependencias de MetaMask y WalletConnect
     webpack: (config) => {
         config.resolve.fallback = { fs: false, net: false, tls: false };
         config.externals.push('pino-pretty', 'lokijs', 'encoding');
         return config;
-    },
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-                port: '',
-                pathname: '/**',
-            },
-        ],
     },
 };
 

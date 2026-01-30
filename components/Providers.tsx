d322a7d74fb121c9661490b7cf7f99361da5b517
Admin <josejordan20222@gmail.com>
2026-01-30 10:10:35 +0100

    fix: Reinstate AppProvider in Providers to resolve build error

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "next-themes";
 import { LanguageProvider } from "@/src/context/LanguageContext";
 import { State } from "wagmi";
 import { SettingsProvider } from "@/src/context/SettingsContext";
+import { AppProvider } from "@/components/AppContext";
 import dynamic from 'next/dynamic';
 
 const ClientWeb3Provider = dynamic(() => import('@/components/ClientWeb3Provider'), {
@@ -14,11 +15,13 @@ export default function Providers({ children, initialState }: { children: React.
     return (
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
             <ClientWeb3Provider cookies={null}>
-                <SettingsProvider>
-                    <LanguageProvider>
-                        {children}
-                    </LanguageProvider>
-                </SettingsProvider>
+                <AppProvider>
+                    <SettingsProvider>
+                        <LanguageProvider>
+                            {children}
+                        </LanguageProvider>
+                    </SettingsProvider>
+                </AppProvider>
             </ClientWeb3Provider>
         </ThemeProvider>
     );
